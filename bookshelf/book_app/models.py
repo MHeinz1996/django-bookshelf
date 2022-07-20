9220091d8055ba8975bf6091374dbbb21274eb87
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-20 13:57:17 -0500

    Cloned Avery's bookshelf program

--- bookshelf/book_app/models.py
+++ bookshelf/book_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length = 255, blank = False)
+    name = models.CharField(max_length = 255, blank = False) # blank false prevents empty entries
 
     def __str__(self):
         return self.name
@@ -13,6 +13,7 @@ class Book(models.Model):
     author = models.CharField(max_length = 255)
     description = models.CharField(max_length = 255)
     genre = models.ForeignKey(Genre, on_delete = models.CASCADE, default = 1)
+    # A book can have ONE genre, but a genre can have MANY books. One-to-Many relationship
 
     def __str__(self):
         return self.title
